0b6c2bbce38ea6d2ac6cdb3c65d22a19804717d6
Gareth Kennedy <gakennedy@guidewire.com>
2017-02-25 20:12:10 +0000

    Black and White Camera Script Implementation

---
+++ Assets/Gareth/G_Scripts/G_BlackAndWhiteCamera/G_BlackAndWhiteCameraScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class G_BlackAndWhiteCameraScript : MonoBehaviour
+{
+    public float intensity;
+    private Material _material;
+
+    //Creates a private meterial used to the effect.
+    void Awake()
+    {
+        _material = new Material(Shader.Find("Hidden/G_BlackAndWhiteCameraShader"));
+    }
+
+    //Postprocess the image.
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if(intensity == 0)
+        {
+            Graphics.Blit(source, destination);
+            return;
+        }
+
+        _material.SetFloat("_bwBlend", intensity);
+        Graphics.Blit(source, destination, _material);
+    }
+}
